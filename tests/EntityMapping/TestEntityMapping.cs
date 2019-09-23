df93e3512af5f1dc4b5b7e0a4f3fee64e9c43c09
Milan <m.drossaerts@gmail.com>
2019-09-23 19:37:13 +0200

    Refactor TestEntityMapping

---
+++ tests/EntityMapping/TestEntityMapping.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Extended.Dapper.Tests.Models;
+using Extended.Dapper.Core.Mappers;
+
+namespace Extended.Dapper.Tests.Query
+{
+    /// <summary>
+    /// This class tests if the mapping of model works correctly
+    /// </summary>
+    [TestFixture]
+    public class TestEntityMapping
+    {
+        public EntityMap AuthorMap { get; set; }
+
+        public EntityMap BookMap { get; set; }
+
+        [OneTimeSetUpAttribute]
+        public void FixtureSetup()
+        {
+            this.AuthorMap  = EntityMapper.GetEntityMap(typeof(Author));
+            this.BookMap    = EntityMapper.GetEntityMap(typeof(Book));
+        }
+
+        /// <summary>
+        /// This tests if all native properties get correctly mapped
+        /// </summary>
+        [Test]
+        [TestCase("Id", typeof(Guid))]
+        [TestCase("Name", typeof(string))]
+        [TestCase("ReleaseYear", typeof(int))]
+        [TestCase("OriginalName", typeof(string))]
+        public void TestNativePropertyMapping(string propertyName, Type expectedType)
+        {
+            var property = this.BookMap.MappedProperties.Single(p => p.Name == propertyName);
+            Assert.AreEqual(expectedType, property.PropertyType, "Property Type is incorrect");
+        }
+
+        /// <summary>
+        /// This tests if the primary keys get properly mapped
+        /// </summary>
+        [Test]
+        public void TestIfPrimaryKeysGetMapped()
+        {
+            // Author
+            var authorPrimaryKeys = this.AuthorMap.PrimaryKeyPropertiesMetadata;
+            var authorPrimaryKey = authorPrimaryKeys.FirstOrDefault();
+
+            Assert.AreEqual(1, authorPrimaryKeys.Count);
+            Assert.AreEqual("Id", authorPrimaryKey.ColumnName);
+            Assert.AreEqual("Id", authorPrimaryKey.PropertyName);
+            Assert.AreEqual(typeof(Guid), authorPrimaryKey.PropertyInfo.PropertyType);
+            Assert.AreEqual(true, authorPrimaryKey.AutoValue);
+        }
+
+        /// <summary>
+        /// This tests if the relations get properly mapped
+        /// </summary>
+        [Test]
+        public void TestIfRelationsGetMapped()
+        {
+            // Author
+            var authorRelations = this.AuthorMap.RelationProperties;
+            Assert.AreEqual(1, authorRelations.Count, "Author does not have 1 relation property mapped");
+            var bookRelation = authorRelations.Single(kv => kv.Key.Name == "Books");
+
+            // Books
+            var booksRelations      = this.BookMap.RelationProperties;
+            Assert.AreEqual(3, booksRelations.Count, "Book does not have 3 relation properties mapped");
+            var authorRelation      = booksRelations.Single(kv => kv.Key.Name == "Author");
+            var coAuthorRelation    = booksRelations.Single(kv => kv.Key.Name == "CoAuthor");
+            var categoryRelation    = booksRelations.Single(kv => kv.Key.Name == "Category");
+        }
+    }
+}
